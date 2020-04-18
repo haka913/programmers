29e4ab2fe6b57d61e0250c10a00a83f0e52e98dc
paul <ace95913@gmail.com>
2020-04-18 21:59:37 +0900

    programmars

--- p_2020KAKAO_가사검색.py
+++
@@ -1,9 +0,0 @@
-def solution(words, queries):
-    answer = []
-    return answer
-
-
-print(solution(words=["frodo", "front", "frost", "frozen", "frame", "kakao"],
-               queries=["fro??", "????o", "fr???", "fro???", "pro?"]))
-
-#Todo: 풀기
--- p_멀쩡한사각형.py
+++
@@ -1,6 +0,0 @@
-# boj 2168 타일 위의 대각선
-import math
-def solution(w,h):
-    return w*h -w-h+math.gcd(w,h)
-
-print(solution(w=8, h=12))
